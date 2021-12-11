ee81cde9964701391742c46612ba3a3ac70b4550
Aakif Raza <simply.aakif@gmail.com>
2021-12-11 13:06:07 +0500

    Working on Batch based online Class

--- app/Mail/StudentUserDetailsMail.php
+++ app/Mail/StudentUserDetailsMail.php
@@ -2,10 +2,13 @@
 
     namespace App\Mail;
 
+    use Illuminate\Bus\Queueable;
+    use Illuminate\Contracts\Queue\ShouldQueue;
     use Illuminate\Mail\Mailable;
 
-    class StudentUserDetailsMail extends Mailable {
+    class StudentUserDetailsMail extends Mailable implements ShouldQueue {
 
+        use Queueable;
         public $details;
 
         public function __construct($details)
