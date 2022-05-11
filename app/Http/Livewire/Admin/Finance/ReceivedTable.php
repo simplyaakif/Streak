a54e1ffcd67b891d211ce087c1e92acadf45e11d
Aakif Raza <simply.aakif@gmail.com>
2022-05-11 16:56:03 +0500

    Received Ordered by Updated

--- app/Http/Livewire/Admin/Finance/ReceivedTable.php
+++ app/Http/Livewire/Admin/Finance/ReceivedTable.php
@@ -25,7 +25,7 @@
                         $course->withTrashed();
                     })
                     ->where('is_paid',1)
-                    ->latest()
+                    ->orderBy('paid_on')
                     ->paginate(50)
             ]);
         }
