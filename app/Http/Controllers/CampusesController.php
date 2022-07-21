f2acf5d71cccf91d50629b9e09c8d84c7a5f32a6
App Soulz <simply.aakif@gmail.com>
2022-07-21 14:46:31 +0500

    Campus Model Added, Fields for Campus & Mode Added to Online Registration Form

---
+++ app/Http/Controllers/CampusesController.php
@@ -0,0 +1,44 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use App\Models\Campus;
+    use Illuminate\Http\Request;
+
+    class CampusesController extends Controller {
+
+        public function index()
+        {
+            //
+        }
+
+        public function create()
+        {
+            //
+        }
+
+        public function store(Request $request)
+        {
+            //
+        }
+
+        public function show(Campus $campus)
+        {
+            //
+        }
+
+        public function edit(Campus $campus)
+        {
+            //
+        }
+
+        public function update(Request $request, Campus $campus)
+        {
+            //
+        }
+
+        public function destroy(Campus $campus)
+        {
+            //
+        }
+    }
