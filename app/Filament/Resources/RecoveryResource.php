669e7a63b794b116bbec85d0cf5f58ae0142d32a
Aakif Raza <simply.aakif@gmail.com>
2023-10-27 15:54:43 +0500

    Excel Export Enabled

--- app/Filament/Resources/RecoveryResource.php
+++ app/Filament/Resources/RecoveryResource.php
@@ -16,6 +16,7 @@ use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class RecoveryResource extends Resource
 {
@@ -117,6 +118,7 @@ class RecoveryResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
+                ExportBulkAction::make()
             ]);
     }
 
