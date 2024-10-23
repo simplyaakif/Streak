d6900d6cb0e6957aba83a606b2437ac280daaafa
muhammadaakifraza <simply.aakif@gmail.com>
2024-10-23 18:34:20 +0500

    Cors Origins Updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://portal.ace.org.pk','http://acev7.local'],
 
     'allowed_origins_patterns' => [],
 
